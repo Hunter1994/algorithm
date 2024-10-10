a299a0d921afb65ba6a0524ae9c712af9e30c619
zhangdi <962410314@qq.com>
2024-10-10 13:04:46 +0800

    update

--- 173/Program.cs
+++ 173/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Solution solution=new Solution ();
+var res=solution.FindLucky([2,2,2,3,3]);
+Console.WriteLine(res);
+
+public class Solution {
+    public int FindLucky(int[] arr) {
+        int[] temp=new int[501];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            temp[arr[i]]++;
+        }
+        
+        for (int i = 500; i >=1; i--)
+        {
+            if(temp[i]==i)return i;
+        }
+return -1;
+    }
+}
